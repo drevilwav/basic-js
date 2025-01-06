63101e7db43c5f4130aa539d17b3bdfe08c566ae
Vladimir <vl.kutepov@gmail.com>
2025-01-06 16:32:20 +1000

    finish: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const stats = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let currentDNS = '';
+
+    for (const part of parts) {
+      currentDNS += `.${part}`;
+      if (stats[currentDNS]) {
+        stats[currentDNS] += 1;
+      } else {
+        stats[currentDNS] = 1;
+      }
+    }
+  }
+  return stats;
 }
 
 module.exports = {
